7531a0c7465b7910c2c4731a635625daddb08ed6
Jianjun Zhu <jianjun.zhu@intel.com>
2020-12-18 10:46:57 +0800

    Remove unused code for doc generation.
    The files listed are not there.

--- docs/gendoc.py
+++ docs/gendoc.py
@@ -20,9 +20,6 @@ STACK_AUDIO_PATH='src/third_party/webrtc/modules/audio_device/android/java/src/c
 ANDROID_REPO_PATH=os.path.join(THIS, '../src/sdk/base/src/main/java/com/intel/webrtc/base/')
 DOXYGEN_PATH = os.path.join(THIS, 'doxygen')
 
-files = ['FilterCallback.java', 'VideoFrameFilterInterface.java']
-audio_files = ['IcsAudioRecord.java']
-
 FOUND_STACK = False
 
 def cleanEnv():
@@ -33,41 +30,14 @@ def cleanEnv():
     if os.path.exists(os.path.join(THIS, 'html')):
         shutil.rmtree(os.path.join(THIS, 'html'))
 
-def copyFiles(path):
-    if not os.path.exists(os.path.join(path, STACK_ANDROID_PATH)):
-        print 'Wrong location for webrtc stack.'
-        return
-
-    global FOUND_STACK
-    FOUND_STACK = True
-    for f in files:
-        cmd = ['cp', os.path.join(path, STACK_ANDROID_PATH, f), os.path.join(ANDROID_REPO_PATH, f)]
-        subprocess.call(cmd)
-    for f in audio_files:
-        cmd = ['cp', os.path.join(path, STACK_AUDIO_PATH, f), os.path.join(ANDROID_REPO_PATH, f)]
-        subprocess.call(cmd)
-
-def rmFiles():
-    for f in files:
-        cmd = ['rm', os.path.join(ANDROID_REPO_PATH, f)]
-        subprocess.call(cmd)
-    for f in audio_files:
-        cmd = ['rm', os.path.join(ANDROID_REPO_PATH, f)]
-        subprocess.call(cmd)
-
 def genDoc():
     os.chdir(DOXYGEN_PATH)
     cmd = ['doxygen', 'doxygen_android.conf']
     subprocess.call(cmd)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-stack_location', default='.', help='webrtc stack location')
-    args = parser.parse_args()
     cleanEnv()
-    copyFiles(args.stack_location)
     genDoc()
-    rmFiles()
     if not FOUND_STACK:
         print '\n ------------------------------'
         print ' | No webrtc stack found      |'
